ae52cc12d965ec5744012c5299b9de97ea07bca8
SiddharthKrM <siddharth05040@gmail.com>
2024-02-29 13:02:02 +0530

    encapsulation solu

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] arg){
+        RWOnly rwOnly=new RWOnly();
+        rwOnly.setName("sid");
+        System.out.println(rwOnly.getName());
+    }
   
 }
